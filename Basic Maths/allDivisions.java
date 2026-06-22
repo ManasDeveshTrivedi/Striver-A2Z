900b108d79926e5aff7ffcd7a10303e330597400
Manas <manasdevtrivedi@gmail.com>
2026-06-22 22:51:47 +0530

    Added

---
+++ Basic Maths/allDivisions.java
@@ -0,0 +1,9 @@
+public class allDivisions {
+    public static void main(String[] args) {
+        int n=36;
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0)System.out.println(i);
+            if((n/i)!=i)System.out.println(n/i);
+        }
+    }
+}
